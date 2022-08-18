15f03c3a842f936ce62caedafbf7a6e887fa09ce
Pavel Derevyankin <pavelmurman@yandex.ru>
2022-08-18 22:37:56 +0300

    Команда "Ask"

--- TgKarBot/Constants/Messages.cs
+++ TgKarBot/Constants/Messages.cs
@@ -9,5 +9,13 @@
             " регистрацию на другое устройство, обратитесь к @Edgar_Allan_Po_2022_Bot";
         internal const string Angry = "КАР! Я НЕ МОГУ РАБОТАТЬ В ТАКИХ УСЛОВИЯХ, ВАС СЛИШКОМ МНОГО! (бот не поддерживает групповые чаты)";
         internal const string DoneTeamRegisteration = "Ваша команда успешно зарегистрирована и закреплена за этим пользователем";
+        internal const string NotRegistered = "Вы не зарегистрированы, как участник, зарегистрируйте," +
+            " пожалуйста, вашу команду или обратитесь к @Edgar_Allan_Po_2022_Bot";
+        internal const string NotCorrectNum = "Неверный номер вопроса";
+        internal const string NotCorrectAsk = "Ответ неверный";
+        internal const string WinTheGame = "Поздравляем вас! Вы прошли игру!";
+        internal const string Correct = "Правильный ответ. За это я вам скажу: ";
+
+        internal const string Error = "Произошла ошибка в работе бота, пожалуйста, свяжитесь с администрацией через @Edgar_Allan_Po_2022_Bot";
     }
 }
--- TgKarBot/Logic/Teams.cs
+++ TgKarBot/Logic/Teams.cs
@@ -9,14 +9,15 @@ namespace TgKarBot.Logic
 {
     internal class Teams
     {
-        public static string Registrate(string commandId, long userId)
+        private static string _teamsPath = Directory.GetCurrentDirectory() + "/teams.txt";
+        private static string _asksPath = Directory.GetCurrentDirectory() + "/asks.txt";
+        private static string _teamsProgressPath = Directory.GetCurrentDirectory() + "/teamsProgress.txt";
+        private static string _rewardsPath = Directory.GetCurrentDirectory() + "/rewards.txt";
+        public static async Task<string> Registrate(string commandId, long userId)
         {
-            string directory = Directory.GetCurrentDirectory();
-            string teamsPath = directory + "/teams.txt";
-
-            if (File.Exists(teamsPath))
+            if (File.Exists(_teamsPath))
             {
-                using var streamReader = new StreamReader(teamsPath);
+                using var streamReader = new StreamReader(_teamsPath);
                 var line = streamReader.ReadLine();
                 
                 while (line != null)
@@ -29,12 +30,70 @@ namespace TgKarBot.Logic
 
                         return Constants.Messages.OtherUser;
                     }
-                    line = streamReader.ReadLine();
+                    line = await streamReader.ReadLineAsync();
                 }
             }
 
-            File.WriteAllLinesAsync(teamsPath, new[] {$"{commandId} {userId}"});
+            using (var file = new StreamWriter(_teamsPath))
+                await file.WriteLineAsync($"{commandId} {userId}");
+
+            using (var file = new StreamWriter(_teamsProgressPath))
+                await file.WriteLineAsync($"{commandId} 0");
             return Constants.Messages.DoneTeamRegisteration;
+        }
+
+        public static async Task<string> Ask(long userId, string message)
+        {
+            if (!File.Exists(_asksPath)) return Constants.Messages.Error;
+
+            using var streamReader = new StreamReader(_teamsPath);
+            var line = streamReader.ReadLine();
+            var commandId = "";
+
+            while (line != null)
+            {
+                var splittedLine = line.Split();
+                if (splittedLine[1] == userId.ToString())
+                {
+                    commandId = splittedLine[0];
+                    break;
+                }
+                               
+                line = await streamReader.ReadLineAsync();
+            }
+
+            if (string.IsNullOrEmpty(commandId))
+                return Constants.Messages.NotRegistered;
+
+            var splittedMessage = message.Split();
+            var num = int.Parse(splittedMessage[0]);
+            var ask = message.Remove(0, splittedMessage[0].Length + 1);
+            var allLinesAsks = await File.ReadAllLinesAsync(_asksPath);
+            if (allLinesAsks.Length < num)
+                return Constants.Messages.NotCorrectNum;
+
+            var correctAsk = allLinesAsks.First(x => x.StartsWith(num.ToString()));
+
+            if (ask != correctAsk)
+                return Constants.Messages.NotCorrectAsk;
+
+            var allLinesProgress = await File.ReadAllLinesAsync(_teamsProgressPath);
+            var currentMagicNumber = int.Parse(allLinesProgress.First(x => x.Contains(commandId)).Split()[1]);
+            currentMagicNumber = (int) Math.Pow(2, num);
+            
+            await File.WriteAllLinesAsync(_teamsProgressPath, allLinesProgress.Where(x => !x.Contains(commandId)));
+
+            using (var file = new StreamWriter(_teamsProgressPath))
+                await file.WriteLineAsync($"{commandId} {currentMagicNumber}");
+
+            var rewards = await File.ReadAllLinesAsync(_teamsProgressPath);
+            var rewardText = rewards.First(x => x.StartsWith(num.ToString())).Remove(0, splittedMessage[0].Length + 1);
+
+            if (Math.Pow(2, allLinesProgress.Length + 1) - 1 == num)
+                return Constants.Messages.WinTheGame + @"\n" + rewardText; // Todo Тут потом надо будет обязательно подумать про финальное сообщение,
+                                                      // я так до сих пор в механике и не уверен
+            return Constants.Messages.Correct + @"\n" + rewardText;
+            
         }
     }
 }
