d3ffeb6f2105a0612da2b296a553f1c73f341c65
derevyankin.pg <pavelmurman@yandex.ru>
2022-08-20 20:00:05 +0300

    синонимы Асков

--- TgKarBot/Constants/Commands.cs
+++ TgKarBot/Constants/Commands.cs
@@ -4,20 +4,21 @@
     {
 
         internal const string Start = "/start";
-        internal static List<string> StartSynonims = new List<string>() { Start, "start", "старт", "начать", "запуск"};
+        internal static List<string> StartSynonims = new() { Start, "start", "старт", "начать", "запуск"};
 
         internal const string RegTeam = "/regteam";
-        internal static List<string> RegTeamSynonims = new List<string>() { RegTeam, "team", "reg", "регистрация", "команда" };
+        internal static List<string> RegTeamSynonims = new() { RegTeam, "team", "reg", "регистрация", "команда" };
         internal const string RegTeamSample = RegTeam + " id";
 
         internal const string Ask = "/ask";
-        internal static List<string> AskSynonims = new List<string>() { Ask, "ask", "Ответ" };
+        internal static List<string> AskSynonims = new() { Ask, "ask", "Ответ" };
         internal const string AskSample = RegTeam + " (номер вопроса) (ответ)";
 
-        internal static List<List<string>> Synonims = new List<List<string>>()
+        internal static List<List<string>> Synonims = new()
         {
             StartSynonims,
-            RegTeamSynonims
+            RegTeamSynonims,
+            AskSynonims
         };
     }
 }
