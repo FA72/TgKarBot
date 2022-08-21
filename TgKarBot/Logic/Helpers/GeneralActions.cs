46bc0998464a7f0b782fdc1be1376d0d8fc0923f
derevyankin.pg <pavelmurman@yandex.ru>
2022-08-21 16:41:01 +0300

    general del

--- TgKarBot/Logic/Helpers/GeneralActions.cs
+++ TgKarBot/Logic/Helpers/GeneralActions.cs
@@ -30,5 +30,27 @@ namespace TgKarBot.Logic.Helpers
 
             return successMessage;
         }
+
+        internal static async Task<string> DeleteSomething(
+            long userId,
+            string message,
+            Func<string, Task<string?>> readFunc,
+            Func<string, Task> deleteFunc,
+            string doesntExistMessage,
+            string successMessage
+        )
+        {
+            if (!await Admins.CheckAdmins(userId)) return Messages.OnlyForAdmins;
+
+            var splittedMessage = message.Split();
+            var num = splittedMessage[1];
+            if (await readFunc(num) == null)
+                return doesntExistMessage;
+
+            var ask = Parser.ParseBodyMessage(splittedMessage, 2);
+            await deleteFunc(num);
+
+            return successMessage;
+        }
     }
 }
