cac29617dedbaae4bd14d4a02e05874656f8cd12
derevyankin.pg <pavelmurman@yandex.ru>
2022-08-21 18:24:11 +0300

    Fix {}

--- TgKarBot/Logic/Admins.cs
+++ TgKarBot/Logic/Admins.cs
@@ -8,40 +8,43 @@ namespace TgKarBot.Logic
         internal static async Task<string> AddTask(long userId, string message)
         {
             return await GeneralActions.AddSomething(
-                    userId, message,
-                    Database.Database.ReadAskAsync,
-                    Database.Database.CreateAskAsync,
-                    Messages.TaskAlreadyExist,
-                    Messages.TaskSuccessCreation,
-                    2);
+                userId, message,
+                Database.Database.ReadAskAsync,
+                Database.Database.CreateAskAsync,
+                Messages.TaskAlreadyExist,
+                Messages.TaskSuccessCreation,
+                2);
         }
 
         internal static async Task<string> DeleteTask(long userId, string message)
         {
             return await GeneralActions.DeleteSomething(
-                    userId, message,
-                    Database.Database.ReadAskAsync,
-                    Database.Database.DeleteAskAsync,
-                    Messages.TaskDoesntExist,
-                    Messages.TaskSuccessDelete);
+                userId, message,
+                Database.Database.ReadAskAsync,
+                Database.Database.DeleteAskAsync,
+                Messages.TaskDoesntExist,
+                Messages.TaskSuccessDelete);
         }
 
         internal static async Task<string> AddAdmin(long userId, string message)
         {
             return await GeneralActions.AddSomething(
-                    userId, message,
-                    Database.Database.ReadAdminAsync,
-                    Database.Database.CreateAdminAsync,
-                    Messages.AdminAlreadyExist,
-                    Messages.AdminSuccessCreation,
-                    1);
+                userId, message,
+                Database.Database.ReadAdminAsync,
+                Database.Database.CreateAdminAsync,
+                Messages.AdminAlreadyExist,
+                Messages.AdminSuccessCreation,
+                1);
         }
 
         internal static async Task<string> DeleteAdmin(long userId, string message)
         {
             return await GeneralActions.DeleteSomething(
-                    userId, message,
-                    Database.Database.ReadAdminAsync,
-                    Database.Database.DeleteAdminAsync,
-                    Messages.AdminDoesntExist,
-                    Messages.AdminSuccessDelete);
+                userId, message,
+                Database.Database.ReadAdminAsync,
+                Database.Database.DeleteAdminAsync,
+                Messages.AdminDoesntExist,
+                Messages.AdminSuccessDelete);
+        }
+    }
+}
