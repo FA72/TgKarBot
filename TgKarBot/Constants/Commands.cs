8f2d2de1d42ee22107e02bcf555ee114a7836e71
derevyankin.pg <pavelmurman@yandex.ru>
2022-09-06 16:45:49 +0300

    String fix

--- TgKarBot/Constants/Commands.cs
+++ TgKarBot/Constants/Commands.cs
@@ -8,17 +8,17 @@
 
         internal const string RegTeam = "/regteam";
         internal static List<string> RegTeamSynonims = new() { RegTeam, "team", "reg", "регистрация", "команда" };
-        internal const string RegTeamSample = RegTeam + " id";
+        internal const string RegTeamSample = $"{RegTeam} id";
 
         internal const string Ask = "/ask";
         internal static List<string> AskSynonims = new() { Ask, "ask", "Ответ" };
-        internal const string AskSample = RegTeam + " (номер команды)";
+        internal const string AskSample = $"{RegTeam} (номер команды)";
 
         internal const string AddAsk = "/addask";
-        internal const string AddAskSample = AddAsk + " (номер вопроса) (ответ)";
+        internal const string AddAskSample = $"{AddAsk} (номер вопроса) (ответ)";
 
         internal const string AddAdmin = "/addadmin";
-        internal const string AddAdminSample = AddAdmin + " UserId";
+        internal const string AddAdminSample = $"{AddAdmin} UserId";
 
         internal static List<List<string>> Synonims = new()
         {
