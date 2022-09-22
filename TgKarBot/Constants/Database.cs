68473a3ce3ccf2ec831bccc90dc1da9c260f4004
Pavel Derevyankin <pavelmurman@yandex.ru>
2022-09-22 20:11:39 +0300

    gitignore и датабейс

--- TgKarBot/Constants/Database.cs
+++ TgKarBot/Constants/Database.cs
@@ -3,8 +3,8 @@
     internal class Database
     {
         public const string ConnectionString =
-            @"Data Source=DESKTOP-33VFDHM;Initial Catalog=TgKarDatabase;Integrated Security=True;Pooling=False";
-            //@"Data Source=DESKTOP-DUT8BI9\SQLEXPRESSROKUS;Initial Catalog=TgKarDatabase;Integrated Security=True";
+            //@"Data Source=DESKTOP-33VFDHM;Initial Catalog=TgKarDatabase;Integrated Security=True;Pooling=False";
+        @"Data Source=DESKTOP-DUT8BI9\SQLEXPRESSROKUS;Initial Catalog=TgKarDatabase;Integrated Security=True";
 
     }
 }
