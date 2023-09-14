52cfe979096acf5428589fae71e13106ee5867fc
Pavel Derevyankin <pavelmurman@yandex.ru>
2023-09-14 19:44:26 +0300

    Доработал модель для EF

--- TgKarBot/Database/Models/TeamProgressModel.cs
+++ TgKarBot/Database/Models/TeamProgressModel.cs
@@ -7,9 +7,13 @@
             TeamId = teamId;
             AskId = askId;
             Time = DateTime.Now;
+            StartDrinkTime = null;
+            EndDrinkTime = null;
         }
         public string TeamId { get; set; }
         public string AskId { get; set; }
         public DateTime Time { get; set; }
+        public DateTime? StartDrinkTime { get; set; }
+        public DateTime? EndDrinkTime { get; set; }
     }
 }
