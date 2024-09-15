216d3f2cb301c32cadb130a3fd65ef686308cdcd
derevyankin.pg <pavelmurman@yandex.ru>
2024-09-15 11:11:48 +0300

    Обновил под реалии кз

--- TgKarBot/Constants/ChatId.cs
+++ TgKarBot/Constants/ChatId.cs
@@ -2,5 +2,5 @@
 
 internal class ChatId
 {
-    internal const long AdminChatId = -4502567900; // Todo введите айди, куда будут приходить ответы бота (предварительно добавьте бота туда)
+    internal const long AdminChatId = -4525017135; // Todo введите айди, куда будут приходить ответы бота (предварительно добавьте бота туда)
 }
